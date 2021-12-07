f86af5c24737706c512d9d296531f68e17dc5ca0
Adam Reeve <adreeve@gmail.com>
2021-12-08 10:04:27 +1300

    Initial type mapping to DataFrame column types

--- ParquetSharp.DataFrame.Test/ParquetToDataFrame.cs
+++ ParquetSharp.DataFrame.Test/ParquetToDataFrame.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Linq;
+using Microsoft.Data.Analysis;
 using ParquetSharp.IO;
 using Xunit;
 
@@ -14,29 +16,35 @@ namespace ParquetSharp.DataFrame.Test
                 new Column<int>("int"),
                 new Column<double>("double"),
                 new Column<string>("string"),
-                new Column<bool>("bool")
+                new Column<bool>("bool"),
+                new Column<DateTime>("dateTime"),
             };
 
             const int numRows = 1000;
-                
+
             using var buffer = new ResizableBuffer();
             using (var output = new BufferOutputStream(buffer))
             {
                 var propertiesBuilder = new WriterPropertiesBuilder();
                 using var fileWriter = new ParquetFileWriter(output, columns, propertiesBuilder.Build());
                 using var rowGroupWriter = fileWriter.AppendRowGroup();
-                
+
                 using var intCol = rowGroupWriter.NextColumn().LogicalWriter<int>();
                 intCol.WriteBatch(Enumerable.Range(0, numRows).ToArray());
-                
+
                 using var doubleCol = rowGroupWriter.NextColumn().LogicalWriter<double>();
                 doubleCol.WriteBatch(Enumerable.Range(0, numRows).Select(i => (double) i).ToArray());
-                
+
                 using var stringCol = rowGroupWriter.NextColumn().LogicalWriter<string>();
                 stringCol.WriteBatch(Enumerable.Range(0, numRows).Select(i => i.ToString()).ToArray());
-                
+
                 using var boolCol = rowGroupWriter.NextColumn().LogicalWriter<bool>();
                 boolCol.WriteBatch(Enumerable.Range(0, numRows).Select(i => i % 2 == 0).ToArray());
+
+                using var dateTimeCol = rowGroupWriter.NextColumn().LogicalWriter<DateTime>();
+                dateTimeCol.WriteBatch(
+                    Enumerable.Range(0, numRows)
+                        .Select(i => new DateTime(2021, 12, 8) + TimeSpan.FromSeconds(i)).ToArray());
 
                 fileWriter.Close();
             }
@@ -45,9 +53,24 @@ namespace ParquetSharp.DataFrame.Test
             {
                 using var fileReader = new ParquetFileReader(input);
                 var dataFrame = fileReader.ToDataFrame();
-                
-                Assert.Equal(4, dataFrame.Columns.Count);
-                    
+
+                Assert.Equal(columns.Length, dataFrame.Columns.Count);
+
+                var intCol = dataFrame["int"];
+                Assert.IsType<PrimitiveDataFrameColumn<int>>(intCol);
+
+                var doubleCol = dataFrame["double"];
+                Assert.IsType<PrimitiveDataFrameColumn<double>>(doubleCol);
+
+                var stringCol = dataFrame["string"];
+                Assert.IsType<StringDataFrameColumn>(stringCol);
+
+                var boolCol = dataFrame["bool"];
+                Assert.IsType<BooleanDataFrameColumn>(boolCol);
+
+                var dateTimeCol = dataFrame["dateTime"];
+                Assert.IsType<PrimitiveDataFrameColumn<DateTime>>(dateTimeCol);
+
                 fileReader.Close();
             }
         }
--- ParquetSharp.DataFrame/ParquetFileReaderExtensions.cs
+++ ParquetSharp.DataFrame/ParquetFileReaderExtensions.cs
@@ -1,10 +1,62 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Data.Analysis;
+
 namespace ParquetSharp.DataFrame
 {
     public static class ParquetFileReaderExtensions
     {
         public static Microsoft.Data.Analysis.DataFrame ToDataFrame(this ParquetFileReader fileReader)
         {
-            return new Microsoft.Data.Analysis.DataFrame();
+            var numColumns = fileReader.FileMetaData.NumColumns;
+            var numRows = fileReader.FileMetaData.NumRows;
+            var columns = new List<DataFrameColumn>(numColumns);
+
+            for (int i = 0; i < numColumns; ++i)
+            {
+                var descriptor = fileReader.FileMetaData.Schema.Column(i);
+                columns.Add(CreateColumn(descriptor, numRows));
+            }
+
+            return new Microsoft.Data.Analysis.DataFrame(columns);
+        }
+
+        private static DataFrameColumn CreateColumn(ColumnDescriptor descriptor, long numRows)
+        {
+            switch (descriptor.LogicalType)
+            {
+                case NoneLogicalType:
+                case IntLogicalType:
+                {
+                    switch (descriptor.PhysicalType)
+                    {
+                        case PhysicalType.Int32:
+                            return new PrimitiveDataFrameColumn<int>(descriptor.Name, numRows);
+                        case PhysicalType.Int64:
+                            return new PrimitiveDataFrameColumn<long>(descriptor.Name, numRows);
+                        case PhysicalType.Float:
+                            return new PrimitiveDataFrameColumn<float>(descriptor.Name, numRows);
+                        case PhysicalType.Double:
+                            return new PrimitiveDataFrameColumn<double>(descriptor.Name, numRows);
+                        case PhysicalType.Boolean:
+                            return new BooleanDataFrameColumn(descriptor.Name, numRows);
+                        default:
+                            throw new NotImplementedException($"Unsupported physical type: {descriptor.PhysicalType}");
+                    }
+                }
+                case StringLogicalType:
+                {
+                    return new StringDataFrameColumn(descriptor.Name, numRows);
+                }
+                case TimestampLogicalType:
+                {
+                    return new PrimitiveDataFrameColumn<DateTime>(descriptor.Name, numRows);
+                }
+                default:
+                {
+                    throw new NotImplementedException($"Unsupported column logical type: {descriptor.LogicalType.Type}");
+                }
+            }
         }
     }
 }
