cb62d4975ea7a56117a7b976ba4f3169bbd91b8e
Adam Reeve <adreeve@gmail.com>
2021-12-08 16:35:35 +1300

    More column writing tests

--- ParquetSharp.DataFrame.Test/DataFrameToParquet.cs
+++ ParquetSharp.DataFrame.Test/DataFrameToParquet.cs
@@ -65,12 +65,90 @@ namespace ParquetSharp.DataFrame.Test
                     VerifyData = (reader, offset) =>
                         VerifyData(reader as LogicalColumnReader<int>, offset, (i, elem) => Assert.Equal(i, elem)),
                 },
+                new TestColumn
+                {
+                    GetColumn = numRows =>
+                        new PrimitiveDataFrameColumn<int>("nullable_int", Enumerable.Range(0, numRows).Select(i => i % 10 == 0 ? (int?) null : i)),
+                    VerifyData = (reader, offset) =>
+                        VerifyData(reader as LogicalColumnReader<int?>, offset, (i, elem) => Assert.Equal(i % 10 == 0 ? null : (int?) i, elem)),
+                },
+                new TestColumn
+                {
+                    GetColumn = numRows =>
+                        new PrimitiveDataFrameColumn<float>("float", Enumerable.Range(0, numRows).Select(i => (float) i)),
+                    VerifyData = (reader, offset) =>
+                        VerifyData(reader as LogicalColumnReader<float>, offset, (i, elem) => Assert.Equal(i, elem)),
+                },
+                new TestColumn
+                {
+                    GetColumn = numRows =>
+                        new PrimitiveDataFrameColumn<float>("nullable_float", Enumerable.Range(0, numRows).Select(i => i % 10 == 0 ? null : (float?) i)),
+                    VerifyData = (reader, offset) =>
+                        VerifyData(reader as LogicalColumnReader<float?>, offset, (i, elem) => Assert.Equal(i % 10 == 0 ? null : (float?) i, elem)),
+                },
+                new TestColumn
+                {
+                    GetColumn = numRows =>
+                        new PrimitiveDataFrameColumn<double>("double", Enumerable.Range(0, numRows).Select(i => (double) i)),
+                    VerifyData = (reader, offset) =>
+                        VerifyData(reader as LogicalColumnReader<double>, offset, (i, elem) => Assert.Equal(i, elem)),
+                },
+                new TestColumn
+                {
+                    GetColumn = numRows =>
+                        new PrimitiveDataFrameColumn<double>("nullable_double", Enumerable.Range(0, numRows).Select(i => i % 10 == 0 ? null : (double?) i)),
+                    VerifyData = (reader, offset) =>
+                        VerifyData(reader as LogicalColumnReader<double?>, offset, (i, elem) => Assert.Equal(i % 10 == 0 ? null : (double?) i, elem)),
+                },
+                new TestColumn
+                {
+                    GetColumn = numRows =>
+                        new PrimitiveDataFrameColumn<bool>("bool", Enumerable.Range(0, numRows).Select(i => i % 2 == 0)),
+                    VerifyData = (reader, offset) =>
+                        VerifyData(reader as LogicalColumnReader<bool>, offset, (i, elem) => Assert.Equal(i % 2 == 0, elem)),
+                },
+                new TestColumn
+                {
+                    GetColumn = numRows =>
+                        new PrimitiveDataFrameColumn<bool>("nullable_bool", Enumerable.Range(0, numRows).Select(i => i % 10 == 0 ? null : (bool?) (i % 2 == 0))),
+                    VerifyData = (reader, offset) =>
+                        VerifyData(reader as LogicalColumnReader<bool?>, offset, (i, elem) => Assert.Equal(i % 10 == 0 ? null : i % 2 == 0, elem)),
+                },
+                new TestColumn
+                {
+                    GetColumn = numRows =>
+                        new StringDataFrameColumn("string", Enumerable.Range(0, numRows).Select(i => i.ToString())),
+                    VerifyData = (reader, offset) =>
+                        VerifyData(reader as LogicalColumnReader<string>, offset, (i, elem) => Assert.Equal(i.ToString(), elem)),
+                },
+                new TestColumn
+                {
+                    GetColumn = numRows =>
+                        new PrimitiveDataFrameColumn<DateTime>("dateTime", Enumerable.Range(0, numRows).Select(i => new DateTime(2021, 12, 8) + TimeSpan.FromSeconds(i))),
+                    VerifyData = (reader, offset) =>
+                        VerifyData(reader as LogicalColumnReader<DateTime>, offset, (i, elem) => Assert.Equal(new DateTime(2021, 12, 8) + TimeSpan.FromSeconds(i), elem)),
+                },
+                new TestColumn
+                {
+                    GetColumn = numRows =>
+                        new DecimalDataFrameColumn("decimal", Enumerable.Range(0, numRows).Select(i => new decimal(i) / 100)),
+                    VerifyData = (reader, offset) =>
+                        VerifyData(reader as LogicalColumnReader<decimal>, offset, (i, elem) => Assert.Equal(new decimal(i) / 100, elem)),
+                },
+                new TestColumn
+                {
+                    GetColumn = numRows =>
+                        new DecimalDataFrameColumn("nullable_decimal", Enumerable.Range(0, numRows).Select(i => i % 10 == 0 ? null : (decimal?) (new decimal(i) / 100))),
+                    VerifyData = (reader, offset) =>
+                        VerifyData(reader as LogicalColumnReader<decimal?>, offset, (i, elem) => Assert.Equal(i % 10 == 0 ? null : (new decimal(i) / 100), elem)),
+                },
             };
         }
 
         private static void VerifyData<TElement>(
             LogicalColumnReader<TElement> columnReader, long offset, Action<long, TElement> verifier)
         {
+            Assert.NotNull(columnReader);
             var buffer = new TElement[columnReader.BufferLength];
             long readerOffset = 0;
             while (columnReader.HasNext)
