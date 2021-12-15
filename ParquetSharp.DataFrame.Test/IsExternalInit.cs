27ee19f7775c80573c74bacd75466fd651f7a1b8
Adam Reeve <adreeve@gmail.com>
2021-12-15 15:52:09 +1300

    Setup GitHub actions CI based on ParquetSharp

---
+++ ParquetSharp.DataFrame.Test/IsExternalInit.cs
@@ -0,0 +1,6 @@
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices
+{
+    // Needed to support init setters for dotnet < 5.0
+    internal static class IsExternalInit {}
+}
